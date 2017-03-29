8a6feb4145c26051f2148cf3419f1baf65abeee8
Albert Reisswich <albert.reisswich@plentymarkets.com>
2017-03-29 11:45:48 +0200

    fix delete account id

--- src/app/permission/views/account-detail-view/account-detail-view.component.ts
+++ src/app/permission/views/account-detail-view/account-detail-view.component.ts
@@ -125,8 +125,8 @@ export class AccountDetailViewComponent extends Locale implements OnInit
         this.isLoading = true;
         this.payPalUiComponent.callLoadingEvent(true);
         
-        let data = {accountId: this._permissionService.currentAccountId};
-        
+        let data = {accountId: this._permissionService.currentAccount.id};
+
         this.service.deleteAccount(data).subscribe(
             response =>
             {
