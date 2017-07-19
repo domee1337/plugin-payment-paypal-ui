f4a478d1e955fc7f817300e9d791af1f56646c66
Dietmar Langhuth <dietmar.langhuth@plentymarkets.com>
2017-07-19 13:43:47 +0200

    INIT for payment-description

--- src/app/settings/service/settings.service.ts
+++ src/app/settings/service/settings.service.ts
@@ -14,16 +14,16 @@ export class SettingsService extends TerraBaseService
     {
         super(_loadingSpinnerService, _http, '/rest/');
     }
-    
+
     // TODO set parameter for paypal plus installment
     public getSettings(payPalMode:string):Observable<any>
     {
         this.setAuthorization();
-        
+
         let url:string;
-        
+
         url = this.url + 'payment/payPal/settings/' + payPalMode;
-        
+
         return this.mapRequest(
             this.http.get(url, {
                 headers: this.headers,
@@ -31,30 +31,30 @@ export class SettingsService extends TerraBaseService
             })
         );
     }
-    
+
     public saveSettings(data:any):Observable<any>
     {
         this.setAuthorization();
-        
+
         let url:string;
-        
+
         url = this.url + 'payment/payPal/settings/';
-        
+
         return this.mapRequest(
             this.http.post(url, data, {headers: this.headers})
         );
     }
-    
+
     public getWebstores():Observable<any>
     {
         this.setAuthorization();
-        
+
         let url:string;
-        
+
         url = this.url + 'webstores/';
-        
+
         //this.headers.set('Authorization', 'Bearer hhPhh4iexOt5hi6mM5AoQTkQy6RmWOBXhaOkjJpW');
-        
+
         return this.mapRequest(
             this.http.get(url, {
                 headers: this.headers,
@@ -62,18 +62,18 @@ export class SettingsService extends TerraBaseService
             })
         );
     }
-    
-    
+
+
     public getShippingCountries():Observable<any>
     {
         this.setAuthorization();
-        
+
         let url:string;
-        
+
         url = this.url + 'orders/shipping/countries/';
-        
+
         //this.headers.set('Authorization', 'Bearer hhPhh4iexOt5hi6mM5AoQTkQy6RmWOBXhaOkjJpW');
-        
+
         return this.mapRequest(
             this.http.get(url, {
                 headers: this.headers,
@@ -81,22 +81,27 @@ export class SettingsService extends TerraBaseService
             })
         );
     }
-    
+
     public getAccounts():Observable<any>
     {
         this.setAuthorization();
-        
+
         let url:string;
-        
+
         url = this.url + 'payment/payPal/accounts/';
-        
+
         //this.headers.set('Authorization', 'Bearer hhPhh4iexOt5hi6mM5AoQTkQy6RmWOBXhaOkjJpW');
-        
+
         return this.mapRequest(
             this.http.get(url, {
                 headers: this.headers,
                 body:    ''
             })
         );
+    }
+
+    public setAuthorization()
+    {
+      this.setToHeader('Authorization','Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjE1NWM3N2M5MWI1ZWQzZTc5ZGFmOTcwYWQ3Y2MyMjZhMDk2YjFlOTA0M2IyYTYwYTg1YTRlZjQxYWQ1NGNiMjRiMDgwMmE3YzJiNWI4MWJhIn0.eyJhdWQiOiIxIiwianRpIjoiMTU1Yzc3YzkxYjVlZDNlNzlkYWY5NzBhZDdjYzIyNmEwOTZiMWU5MDQzYjJhNjBhODVhNGVmNDFhZDU0Y2IyNGIwODAyYTdjMmI1YjgxYmEiLCJpYXQiOjE1MDA0NTMxNjMsIm5iZiI6MTUwMDQ1MzE2MywiZXhwIjoxNTAwNTM5NTYzLCJzdWIiOiIzIiwic2NvcGVzIjpbIioiXX0.gugqFUWn0QrdqghOsABQaTCu-GYbSfsmnNn3AoqoFktVSo217gkI9I5vhVf0yTMEF7HAXnV2eVUkEApf7oOun7XCiZhOMa_h8A1H1U4sHJvsBNJ9GKTs0ZewzjW8lesbEjZiHkwGjq0Jiq0rbNyCzrcvo4SYXTCVgcXfqhGqysuB0MqqlySxJb37UT_Fcs3wGv-KVpqatO4VQIwf8cJcFsa52UUmlGDlA_F7ZhdavHFmN1hwNo_3IMPSAAcKfcnIyxjKrxNrgIiHT9wU2oVCwlchtBUxDiKBSSp-cDrMJHA_lrQtCuXZSjo-g7yQzok2gy9y7JoSUr0VyAMB156TPhxMSz8lJsjjmyy8OIhY_ErPY3yuuASnr-ntB0xet9ye3mcbmH47cZYhuMIXlSI_oBuDFp0N-fVTPYVkPqI5lhHORmS0MDk2JrGRMCMvTXs2cyXd5mP-0JawJYmk0CqG1NpTIJgibOaKNkY6f19GglsgdiQe2Mj7mNd1YX1-HdX43PQWgJsRqgbYiz0WbyOygEGF4lIquzM0JHY7rr9Pu6DxcWiNYRs2_OL9WrWeNMlgifE7uag2BaedHiKUoXlixBePpGh3qzGh63-YNSRGun6uQ02uEFjNz2TF7dUelmVlgsAsbDaePYy9rPdPongHG7mV-7RL5Ga-A3HixRTvSik');
     }
 }
--- src/app/settings/views/setting-view/setting-view.component.ts
+++ src/app/settings/views/setting-view/setting-view.component.ts
@@ -24,53 +24,54 @@ export class SettingViewComponent extends Locale implements OnInit
 {
     private service:SettingsService;
     private settings;
-    
+
     private selectLang = 'de';
     private webstore;
     private name;
     private logo;
     private infoPage;
+    private description;
     private markup;
-    
+
     private payPalMode:string;
-    
+
     private isLoading:boolean = true;
     private languageValues:Array<TerraSelectBoxValueInterface>;
     private infoPageValues:Array<TerraSelectBoxValueInterface>;
     private logoValues:Array<TerraSelectBoxValueInterface>;
-    
+
     //********************
     private _selectedAccount;
     private _accountListValues:Array<TerraSelectBoxValueInterface> = [];
-    
+
     private _selectedPriority;
     private _priorityListValues:Array<TerraSelectBoxValueInterface> = [];
-    
+
     private _payPalPlus:boolean;
     private _payPalInstallment:boolean;
-    
+
     private _displayNameValue:string;
-    
+
     private _selectedShippingCountry;
     private _shippingCountryListValues:Array<TerraSelectBoxValueInterface> = [];
-    
-    
+
+
     @Input() parameter:any;
-    
+
     constructor(private settingsService:SettingsService,
                 @Inject(forwardRef(() => PayPalUiComponent)) private payPalUiComponent:PayPalUiComponent,
                 locale:LocaleService,
                 localization:LocalizationService)
     {
         super(locale, localization);
-        
+
         this.service = settingsService;
-        
+
         this.settings =
             {
                 webstore: []
             };
-        
+
         this.markup = {
             webstore:  {
                 flatDomestic:       0,
@@ -85,7 +86,7 @@ export class SettingViewComponent extends Locale implements OnInit
                 percentageForeign:  0,
             }
         };
-        
+
         this.languageValues = [
             {
                 value:   'de',
@@ -104,7 +105,7 @@ export class SettingViewComponent extends Locale implements OnInit
                 caption: this.localization.translate('spanish')
             }
         ];
-        
+
         this.infoPageValues = [
             {
                 value:   '0',
@@ -119,7 +120,9 @@ export class SettingViewComponent extends Locale implements OnInit
                 caption: this.localization.translate('externalInfoPage')
             }
         ];
-        
+
+        this.description = '';
+
         this.logoValues = [
             {
                 value:   '0',
@@ -134,7 +137,7 @@ export class SettingViewComponent extends Locale implements OnInit
                 caption: this.localization.translate('donotShowLogo')
             }
         ];
-        
+
         this._priorityListValues = [
             {
                 value:   '1',
@@ -162,7 +165,7 @@ export class SettingViewComponent extends Locale implements OnInit
                 icon:    'icon-rating_small_5'
             }
         ];
-        
+
         this._accountListValues = [
             {
                 value:   '',
@@ -170,7 +173,7 @@ export class SettingViewComponent extends Locale implements OnInit
             }
         ];
     }
-    
+
     /*
      * belong to OnInit Lifecycle hook
      * get called right after the directive's data-bound properties have been checked for the
@@ -187,7 +190,7 @@ export class SettingViewComponent extends Locale implements OnInit
         {
             this.webstore = this.parameter.PID;
         }
-        
+
         this.loadAccounts();
     }
 
@@ -211,16 +214,16 @@ export class SettingViewComponent extends Locale implements OnInit
                                    });
                                }
                            }
-            
+
                            this._accountListValues = items;
-            
+
                            this.payPalUiComponent.callLoadingEvent(false);
                            this.payPalUiComponent.isLoading = false;
                            //this.isLoading = false;
-            
+
                            this.loadShippingCountries();
                        },
-        
+
                        error =>
                        {
                            this.payPalUiComponent.callLoadingEvent(false);
@@ -228,15 +231,15 @@ export class SettingViewComponent extends Locale implements OnInit
                            this.payPalUiComponent.isLoading = false;
                            this.isLoading = false;
                        });
-        
+
     }
-    
+
     public loadShippingCountries()
     {
         this.payPalUiComponent.callLoadingEvent(true);
-        
+
         let value:Array<TerraSelectBoxValueInterface> = [];
-        
+
         this.service
             .getShippingCountries()
             .subscribe(response =>
@@ -251,13 +254,13 @@ export class SettingViewComponent extends Locale implements OnInit
                                                                });
                                                 }
                                             });
-            
+
                            this._shippingCountryListValues = value;
-            
+
                            this.payPalUiComponent.callLoadingEvent(false);
                            this.payPalUiComponent.isLoading = false;
                            //this.isLoading = false;
-            
+
                            this.loadSettings();
                        },
                        error =>
@@ -269,23 +272,23 @@ export class SettingViewComponent extends Locale implements OnInit
                        }
             );
     }
-    
+
     public loadSettings()
     {
         this.payPalUiComponent.callLoadingEvent(true);
-        
+
         this.service
             .getSettings(this.payPalMode)
             .subscribe(
                 response =>
                 {
                     this.mapSettings(response);
-                
+
                     this.payPalUiComponent.callLoadingEvent(false);
                     this.payPalUiComponent.isLoading = false;
                     this.isLoading = false;
                 },
-            
+
                 error =>
                 {
                     this.payPalUiComponent.callLoadingEvent(false);
@@ -295,22 +298,22 @@ export class SettingViewComponent extends Locale implements OnInit
                 }
             );
     }
-    
+
     private mapSettings(response:any):void
     {
         if(typeof response !== 'undefined')
         {
             let settings = this.settings;
-            
+
             for(let res in response)
             {
                 for(let store in response[res])
                 {
                     let PID = store.substr(4);
                     let aktStore = response[res][store];
-                    
+
                     this.checkWebstoreExist(PID);
-                    
+
                     if('markup' in aktStore)
                     {
                         if('webstore' in aktStore.markup)
@@ -320,7 +323,7 @@ export class SettingViewComponent extends Locale implements OnInit
                             settings.webstore[store].markup.webstore.percentageDomestic = aktStore.markup.webstore.percentageDomestic;
                             settings.webstore[store].markup.webstore.percentageForeign = aktStore.markup.webstore.percentageForeign;
                         }
-                        
+
                         if('statistic' in aktStore.markup)
                         {
                             settings.webstore[store].markup.statistic.flatDomestic = aktStore.markup.statistic.flatDomestic;
@@ -329,17 +332,17 @@ export class SettingViewComponent extends Locale implements OnInit
                             settings.webstore[store].markup.statistic.percentageForeign = aktStore.markup.statistic.percentageForeign;
                         }
                     }
-                    
+
                     if('priority' in aktStore)
                     {
                         settings.webstore[store].priority = aktStore.priority;
                     }
-                    
+
                     if('shippingCountries' in aktStore)
                     {
                         settings.webstore[store].shippingCountries = aktStore.shippingCountries;
                     }
-                    
+
                     if('language' in aktStore)
                     {
                         for(let lang in aktStore.language)
@@ -348,28 +351,33 @@ export class SettingViewComponent extends Locale implements OnInit
                             settings.webstore[store].language[lang] = aktStore.language[lang];
                         }
                     }
-                    
+
                     if('account' in aktStore)
                     {
                         settings.webstore[store].account = aktStore.account;
                     }
-                    
+
                     if('payPalPlus' in aktStore)
                     {
                         settings.webstore[store].payPalPlus = aktStore.payPalPlus;
                     }
-                    
+
                     if('calcFinancing' in aktStore)
                     {
                         settings.webstore[store].calcFinancing = aktStore.calcFinancing;
                     }
+
+                    if('description' in aktStore)
+                    {
+                      settings.webstore[store].description = aktStore.description;
+                    }
                 }
             }
         }
-        
+
         this.setCurrentData(this.webstore, this.selectLang);
     }
-    
+
     private checkWebstoreExist(webstore)
     {
         if(this.settings.webstore["PID_" + webstore] === undefined)
@@ -395,11 +403,12 @@ export class SettingViewComponent extends Locale implements OnInit
                     shippingCountries: [],
                     account:           0,
                     payPalPlus:        0,
-                    calcFinancing:     0
+                    calcFinancing:     0,
+                    description:       '',
                 };
         }
     }
-    
+
     private setCurrentData(webstore = null, lang = null)
     {
         if(webstore && lang)
@@ -407,13 +416,13 @@ export class SettingViewComponent extends Locale implements OnInit
             this.webstore = webstore;
             this.selectLang = lang;
         }
-        
+
         let store = "PID_" + this.webstore;
-        
+
         if(store in this.settings.webstore)
         {
             this._selectedPriority = this.settings.webstore[store].priority;
-            
+
             this.markup = {
                 webstore:  {
                     flatDomestic:       this.settings.webstore[store].markup.webstore.flatDomestic,
@@ -428,24 +437,26 @@ export class SettingViewComponent extends Locale implements OnInit
                     percentageForeign:  this.settings.webstore[store].markup.statistic.percentageForeign,
                 }
             };
-            
+
             this._selectedShippingCountry = this.settings.webstore[store].shippingCountries;
-            
+
             if(this.selectLang in this.settings.webstore[store].language)
             {
                 this.infoPage = this.settings.webstore[store].language[this.selectLang].infoPage;
                 this._displayNameValue = this.settings.webstore[store].language[this.selectLang].name;
                 this.logo = this.settings.webstore[store].language[this.selectLang].logo;
+                this.logo = this.settings.webstore[store].language[this.selectLang].description;
             }
             else
             {
                 this.infoPage = "0";
                 this.logo = "0";
                 this._displayNameValue = "";
+                this.description = '';
             }
-            
+
             this._selectedAccount = this.settings.webstore[store].account;
-            
+
             if(this.settings.webstore[store].payPalPlus == 1)
             {
                 this._payPalPlus = true;
@@ -454,7 +465,7 @@ export class SettingViewComponent extends Locale implements OnInit
             {
                 this._payPalPlus = false;
             }
-            
+
             if(this.settings.webstore[store].calcFinancing == 1)
             {
                 this._payPalInstallment = true;
@@ -465,30 +476,30 @@ export class SettingViewComponent extends Locale implements OnInit
             }
         }
     }
-    
+
     private saveSettings():void
     {
         this.isLoading = true;
         this.payPalUiComponent.callLoadingEvent(true);
-        
+
         this.setGeneralData();
         this.setLanguageData();
         this.setMarkupData();
-        
+
         let webstores = [];
-        
+
         for(let store in this.settings.webstore)
         {
             webstores.push({
                                [store]: this.settings.webstore[store]
                            });
         }
-        
+
         let data = {
             PayPalMode: this.payPalMode,
             settings:   webstores
         };
-        
+
         this.service.saveSettings(data).subscribe(
             response =>
             {
@@ -496,7 +507,7 @@ export class SettingViewComponent extends Locale implements OnInit
                 this.payPalUiComponent.callLoadingEvent(false);
                 this.isLoading = false;
             },
-            
+
             error =>
             {
                 this.payPalUiComponent.callStatusEvent(this.localization.translate('errorSaveSettings') + ': ' + error.statusText, 'danger');
@@ -505,7 +516,7 @@ export class SettingViewComponent extends Locale implements OnInit
             }
         );
     }
-    
+
     private resetSettings():void
     {
         this._selectedPriority = 1;
@@ -513,7 +524,7 @@ export class SettingViewComponent extends Locale implements OnInit
         this._payPalInstallment = false;
         this._displayNameValue = "";
         this._selectedShippingCountry = [];
-        
+
         this.markup = {
             webstore:  {
                 flatDomestic:       0,
@@ -529,11 +540,11 @@ export class SettingViewComponent extends Locale implements OnInit
             }
         };
     }
-    
+
     private getSelectedShippingCountries():Array<any>
     {
         let shippingCountryList = [];
-        
+
         this._shippingCountryListValues.forEach((country) =>
                                                 {
                                                     //if(country.active === true)
@@ -541,10 +552,10 @@ export class SettingViewComponent extends Locale implements OnInit
                                                     //    shippingCountryList.push(country.value);
                                                     //}
                                                 });
-        
+
         return shippingCountryList;
     }
-    
+
     private changeWebstore(event:TerraSelectBoxValueInterface)
     {
         //        if(this.webstore != event.value)
@@ -553,38 +564,38 @@ export class SettingViewComponent extends Locale implements OnInit
          * General settings
          */
         this.setGeneralData();
-        
+
         /**
          * Language settings
          */
         this.setLanguageData();
-        
+
         /**
          * Markup settings
          */
         this.setMarkupData();
-        
+
         //        }
         this.webstore = event.value;
-        
+
         this.setCurrentData();
     }
-    
+
     private changeLanguage(event:TerraSelectBoxValueInterface)
     {
         if(this.selectLang != event.value)
         {
             this.setLanguageData();
         }
-        
+
         this.selectLang = event.value;
         this.setCurrentData();
     }
-    
+
     private checkLanguageExist(webstore, language)
     {
         this.checkWebstoreExist(webstore);
-        
+
         if(this.settings.webstore["PID_" + webstore.toString()].language[language] === undefined)
         {
             this.settings.webstore["PID_" + webstore].language[language] =
@@ -595,21 +606,21 @@ export class SettingViewComponent extends Locale implements OnInit
                 };
         }
     }
-    
+
     private setGeneralData()
     {
         /**
          * Check if object is correct
          */
         this.checkWebstoreExist(this.webstore);
-        
+
         /**
          * General settings
          */
         this.settings.webstore["PID_" + this.webstore].priority = this._selectedPriority;
         this.settings.webstore["PID_" + this.webstore].shippingCountries = this._selectedShippingCountry;
         this.settings.webstore["PID_" + this.webstore].account = this._selectedAccount;
-        
+
         if(this._payPalPlus == true)
         {
             this.settings.webstore["PID_" + this.webstore].payPalPlus = 1;
@@ -618,7 +629,7 @@ export class SettingViewComponent extends Locale implements OnInit
         {
             this.settings.webstore["PID_" + this.webstore].payPalPlus = 0;
         }
-        
+
         if(this._payPalInstallment == true)
         {
             this.settings.webstore["PID_" + this.webstore].calcFinancing = 1;
@@ -628,29 +639,30 @@ export class SettingViewComponent extends Locale implements OnInit
             this.settings.webstore["PID_" + this.webstore].calcFinancing = 0;
         }
     }
-    
+
     private setLanguageData()
     {
         /**
          * Check if object is correct
          */
         this.checkLanguageExist(this.webstore, this.selectLang);
-        
+
         /**
          * Language settings
          */
         this.settings.webstore["PID_" + this.webstore].language[this.selectLang].name = this._displayNameValue;
         this.settings.webstore["PID_" + this.webstore].language[this.selectLang].logo = this.logo;
         this.settings.webstore["PID_" + this.webstore].language[this.selectLang].infoPage = this.infoPage;
+        this.settings.webstore["PID_" + this.webstore].language[this.selectLang].description = this.description;
     }
-    
+
     private setMarkupData()
     {
         /**
          * Check if object is correct
          */
         this.checkWebstoreExist(this.webstore);
-        
+
         /**
          * Markup settings
          */
@@ -663,6 +675,6 @@ export class SettingViewComponent extends Locale implements OnInit
         this.settings.webstore["PID_" + this.webstore].markup.statistic.percentageDomestic = this.markup.statistic.percentageDomestic;
         this.settings.webstore["PID_" + this.webstore].markup.statistic.percentageForeign = this.markup.statistic.percentageForeign;
     }
-    
-    
+
+
 }
